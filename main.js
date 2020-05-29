6c9008673471b33bfdf6b4a93c3d4cccb5afedac
JStrummer <64981369+JStrummer@users.noreply.github.com>
2020-05-29 19:12:58 +0200

    Update main.js

--- main.js
+++ main.js
@@ -13,6 +13,10 @@ window.onresize = function () {
   body.style.height = winHeight + 'px';
   body.style.width = winWidth + 'px';
 }
+// Card prototype
+function Card() {
+  this.c0 = [], this.c1 = [], this.c2 = [], this.c3 = [], this.c4 = [], this.c5 = [], this.c6 = [], this.c7 = [], this.c8 = []
+}
 // global bindings
 var littleCard1 = new Card();
 var littleCard2 = new Card();
@@ -21,21 +25,6 @@ var littleCard4 = new Card();
 var littleCard5 = new Card();
 var littleCard6 = new Card();
 var bigCard = [littleCard1,littleCard2,littleCard3,littleCard4,littleCard5,littleCard6];
-var unpulledNumbers = [];
-var unvalidNumbers = [];
-let numberPool = [];
-const lastNumber = {output: '', value: undefined, count: 0, history: []};
-const smorfia = {
-   1: `L'ITALIA`,  2: `'A PICCERELLA`, 3: `'A JATTA`, 4: `'O PUORCO`, 5: `'A MANO`, 6: `CHELLA CA GARDA 'NTERRA`, 7: `'O VASO`, 8: `'A MARONNA`, 9: `'A FIGLIATA`,
-  10: `'E FASULE`, 11: `'E SURICILLE`, 12: `'O SURDATE`, 13: `SANT'ANTONIO`, 14: `'MBRIACO`, 15: `'O GUAGLIONE`, 16: `'O CULO`, 17: `'A DISGRAZZIA`, 18: `'O SANGHE`, 19: `'A RESATA`,
-  20: `'A FESTA`, 21: `'A FEMMENA ANNURA`, 22: `'O PAZZO`, 23: `'O SCEMO`, 24: `'E GGUARDIE`, 25: `NATALE`, 26: `NANNINELLA`, 27: `'O CANTERO`, 28: `'E ZZIZZE`, 29: `'O PATE D'E CCRIATURE`,
-  30: `'E PPALLE D'O TENENTE`, 31: `'O PADRONE 'E CASA`, 32: `'O CAPITONE`, 33: `LL'ANNE 'E CRISTO`, 34: `'A CAPA`, 35: `L'AUCELLUZZ`, 36: `'E CASTAGNELLE`, 37: `'O MONACO`, 38: `'E MMAZZATE`, 39: `'A FUNA N'GANNA`,
-  40: `'A PAPOSCIA`, 41: `'O CURTIELLO`, 42: `O' CAFE'`, 43: `'ONNA PARETA FORE 'O BARCONE`, 44: `MIAAAAAAOOOO`, 45: `'O VINO BBUONO`, 46: `'E DENARE`, 47: `'O MUORTO`, 48: `'O MUORTO CHE PPARLA`, 49: `'O PIEZZO 'E CARNE`,
-  50: `'O PPANE`, 51: `'O CIARDINO`, 52: `'A MADRE`, 53: `'O VIECCHIO`, 54: `'O CAPPIELLO`, 55: `'A MUSECA`, 56: `'A CARUTA`, 57: `'O SCARTELLATO`, 58: `'O PACCOTTO`, 59: `'E PILE`,
-  60: `SE LAMENTA`, 61: `'O CACCIATORE`, 62: `'O MUORTO ACCISO`, 63: `'A SPOSA`, 64: `'A SCIAMMERIA`, 65: `'O CHIANTO`, 66: `'E DDOIE ZETELLE`, 67: `'O TOTARO INT'A CHITARRA`, 68: `'A ZUPPA COTTA`, 69: `SOTT'E 'NCOPPA`,
-  70: `'O PALAZZO`, 71: `L'OMMO 'E MMERDA`, 72: `'A MARAVIGLIA`, 73: `'O SPITALE`, 74: `'A ROTTA`, 75: `PULCINELLA`, 76: `'A FUNTANA`, 77: `'E RRRIAVULE`, 78: `'A BBELLA FIGLIOLA`, 79: `'O MARIUOLO`,
-  80: `'A VOCCA`, 81: `'E SCIURE`, 82: `'A TAVULA 'MBANDITA`, 83: `'O MALETIEMPO`, 84: `'A CCHIESA`, 85: `'LL ANEME 'O PRIATORIO`, 86: `'A PUTECA`, 87: `'E PERUCCHIE`, 88: `'E CASECAVALLE`, 89: `'A VECCHIA`, 90: `'A PAURA`
-}
 
 // HTML Elements
 const bigCardAi = document.querySelectorAll('#ai');
@@ -83,10 +72,6 @@ function createTable() {
   }
 }
 
-// Card prototype
-function Card() {
-  this.c0 = [], this.c1 = [], this.c2 = [], this.c3 = [], this.c4 = [], this.c5 = [], this.c6 = [], this.c7 = [], this.c8 = []
-}
 
 function assignDec (n) {
   if (n >= 1 && n <= 9) return 'c0';
@@ -101,6 +86,7 @@ function assignDec (n) {
 }
 
 // create 90 random numbers and obj { dec:[0,1,2] } and save it to global bindings bigCard
+var unpulledNumbers = [], unvalidNumbers = [];
 let randomBigCard = function() {
   unpulledNumbers = createArray(1,90);
   for (let blankCard of bigCard) {
@@ -221,13 +207,24 @@ function randomBigCardToHtml (e) {
     }
   }
 }
+const smorfia = {
+   1: `L'ITALIA`,  2: `'A PICCERELLA`, 3: `'A JATTA`, 4: `'O PUORCO`, 5: `'A MANO`, 6: `CHELLA CA GARDA 'NTERRA`, 7: `'O VASO`, 8: `'A MARONNA`, 9: `'A FIGLIATA`,
+  10: `'E FASULE`, 11: `'E SURICILLE`, 12: `'O SURDATE`, 13: `SANT'ANTONIO`, 14: `'MBRIACO`, 15: `'O GUAGLIONE`, 16: `'O CULO`, 17: `'A DISGRAZZIA`, 18: `'O SANGHE`, 19: `'A RESATA`,
+  20: `'A FESTA`, 21: `'A FEMMENA ANNURA`, 22: `'O PAZZO`, 23: `'O SCEMO`, 24: `'E GGUARDIE`, 25: `NATALE`, 26: `NANNINELLA`, 27: `'O CANTERO`, 28: `'E ZZIZZE`, 29: `'O PATE D'E CCRIATURE`,
+  30: `'E PPALLE D'O TENENTE`, 31: `'O PADRONE 'E CASA`, 32: `'O CAPITONE`, 33: `LL'ANNE 'E CRISTO`, 34: `'A CAPA`, 35: `L'AUCELLUZZ`, 36: `'E CASTAGNELLE`, 37: `'O MONACO`, 38: `'E MMAZZATE`, 39: `'A FUNA N'GANNA`,
+  40: `'A PAPOSCIA`, 41: `'O CURTIELLO`, 42: `O' CAFE'`, 43: `'ONNA PARETA FORE 'O BARCONE`, 44: `MIAAAAAAOOOO`, 45: `'O VINO BBUONO`, 46: `'E DENARE`, 47: `'O MUORTO`, 48: `'O MUORTO CHE PPARLA`, 49: `'O PIEZZO 'E CARNE`,
+  50: `'O PPANE`, 51: `'O CIARDINO`, 52: `'A MADRE`, 53: `'O VIECCHIO`, 54: `'O CAPPIELLO`, 55: `'A MUSECA`, 56: `'A CARUTA`, 57: `'O SCARTELLATO`, 58: `'O PACCOTTO`, 59: `'E PILE`,
+  60: `SE LAMENTA`, 61: `'O CACCIATORE`, 62: `'O MUORTO ACCISO`, 63: `'A SPOSA`, 64: `'A SCIAMMERIA`, 65: `'O CHIANTO`, 66: `'E DDOIE ZETELLE`, 67: `'O TOTARO INT'A CHITARRA`, 68: `'A ZUPPA COTTA`, 69: `SOTT'E 'NCOPPA`,
+  70: `'O PALAZZO`, 71: `L'OMMO 'E MMERDA`, 72: `'A MARAVIGLIA`, 73: `'O SPITALE`, 74: `'A ROTTA`, 75: `PULCINELLA`, 76: `'A FUNTANA`, 77: `'E RRRIAVULE`, 78: `'A BBELLA FIGLIOLA`, 79: `'O MARIUOLO`,
+  80: `'A VOCCA`, 81: `'E SCIURE`, 82: `'A TAVULA 'MBANDITA`, 83: `'O MALETIEMPO`, 84: `'A CCHIESA`, 85: `'LL ANEME 'O PRIATORIO`, 86: `'A PUTECA`, 87: `'E PERUCCHIE`, 88: `'E CASECAVALLE`, 89: `'A VECCHIA`, 90: `'A PAURA`
+}
 
+const lastNumber = {output: '', value: undefined, count: 0, history: []};
+let numberPool = [];
 function newNumberPool() {numberPool = createArray(1,90)};
 
 function pullNumber () {
-
   if (numberPool.length < 1) {lastNumber.output = 'ULTIMO NUMERO!!!'}
-
   lastNumber.value = randomN(numberPool);
   lastNumber.count++;
   document.getElementById('pulled').textContent = lastNumber.count;
